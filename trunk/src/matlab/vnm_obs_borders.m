d0bfb2507a164e54987de4fd45f060752cc28430
AGDavydov81@gmail.com <AGDavydov81@gmail.com@00ba81bc-6f9e-7aff-8750-0b27046601fb>
2012-09-25 22:07:26 +0000

    [-] borders calculation via grundton

--- trunk/src/matlab/vnm_obs_borders.m
+++
@@ -1,9 +0,0 @@
-function obs=vnm_obs_borders(x, alg, algs, etc_info) %#ok<INUSD>
-	if isfield(alg,'normalize') && alg.normalize
-		x=x*0.95/max(abs(x));
-	end
-
-	marks=emosdk_wrp('grundton3.dll',x,algs.obs_general.fs,3,alg.grundton3_cfg);
-	obs=marks{1};
-	obs(:,1)=obs(:,1)/algs.obs_general.fs;
-end
