648bd64c8711a5bc8a3165758e60dbc6f9cfa64c
andrew.aka.manik <andrew.aka.manik@00ba81bc-6f9e-7aff-8750-0b27046601fb>
2012-11-17 22:19:02 +0000

    [*] слегка подправил функцию смешивания шума и звука

--- trunk/src/matlab/vnm_add_noise_to_signal.m
+++ trunk/src/matlab/vnm_add_noise_to_signal.m
@@ -1,19 +1,31 @@
 function vnm_add_noise_to_signal(signal_file,noise_file, out_directory,SNR_dB)
-	SNR=10^(SNR_dB/10);
 	[x,fs]=wavread(signal_file);
 	[n,fn]=wavread(noise_file);
-	n=resample(n,fs,fn);
-	nmin=min(length(x),length(n));
-	n=n(1:nmin);
-	x=x(1:nmin);
+
+	if fn~=fs
+		n=resample(n,fs,fn);
+	end
+
+	if length(x)>length(n)
+		n = repmat(n, ceil(length(x)/length(n)), 1);
+	end
+	if length(x)<length(n)
+		n(length(x)+1:end)=[];
+	end
+
+	SNR=10^(SNR_dB/10);
 	SNR0=mean(x.*x)/mean(n.*n);
+
 	a=sqrt(SNR/(SNR+1));
 	b=sqrt(SNR0/(SNR+1));
 	y=a*x+b*n;
+
 	ym=max(abs(y));
-	y=y/ym*0.9;
-	nsf=ls(signal_file);
-	nnf=ls(noise_file);
-	new_nsf=strrep(nsf,'.wav',['_' num2str(SNR_dB) 'dB_' nnf(1) '.wav']);
-	wavwrite(y,fs,[out_directory filesep new_nsf]);
+	if ym>0.9
+		y = y*0.9/ym;
+	end
+
+	[~,nsf]=fileparts(signal_file);
+	[~,nnf]=fileparts(noise_file);
+	wavwrite(y,fs,[out_directory filesep nsf '_' num2str(SNR_dB) 'dB_' nnf '.wav']);
 end
