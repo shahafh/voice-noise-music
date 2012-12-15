60e94f742ee08e99953b02f02b6632eb54b6f659
andrew.aka.manik <andrew.aka.manik@00ba81bc-6f9e-7aff-8750-0b27046601fb>
2012-12-15 03:04:51 +0000

    [*] Изменил способ оценки эффективности статистических функционалов: теперь не строятся вырожденные функции распределения, а используются собственно значения функционалов. [*] Значительно оптимизировал по времени работы процедуру определения эффективности наблюдений. [-] параметр alg.examine_obs.base_auto_balance удален за ненадобностью

--- trunk/src/matlab/vnm_examine_obs.m
+++ trunk/src/matlab/vnm_examine_obs.m
@@ -19,7 +19,9 @@ function vnm_examine_obs(base, db_type, alg)
 	
 	% make output subdir name
 	pict.root=[alg.examine_obs.out_dir filesep db_type];
-	mkdir(pict.root);
+	if ~exist(pict.root,'dir')
+		mkdir(pict.root);
+	end
 
 	pict.obs_types=fieldnames(base(1).data{1});
 	pict.obs_types(cellfun(@(x) not(isnumeric(base(1).data{1}.(x))), pict.obs_types))=[];
@@ -35,7 +37,9 @@ function vnm_examine_obs(base, db_type, alg)
 
 	if pict.make_pic_or_fig
 		for di=1:length(pict.obs_types)
-			mkdir(pict.root, pict.obs_types{di});
+			if ~exist([pict.root filesep pict.obs_types{di}],'dir')
+				mkdir(pict.root, pict.obs_types{di});
+			end
 		end
 
 		fig_lin=figure('Visible','off');
@@ -49,7 +53,11 @@ function vnm_examine_obs(base, db_type, alg)
 
 	exam_log=cell(size(obs_name_ch_list));
 
-	usepool = isfield(alg,'matlabpool');
+	if pict.make_pic_or_fig
+		usepool = false;
+	else
+		usepool = isfield(alg,'matlabpool');
+	end
 	if usepool
 		if matlabpool('size')>0
 			matlabpool('close');
@@ -72,22 +80,32 @@ function vnm_examine_obs(base, db_type, alg)
 		end
 	end
 
-	%% PARFOR removed due to lib_svm internal parallelisation
-	fh_log=fopen(log_file_name,'a');
-	for obs_name_ch_list_i=1:length(obs_name_ch_list)
-		exam_obs=struct('name',obs_name_ch_list(obs_name_ch_list_i).obs_name, 'ch',obs_name_ch_list(obs_name_ch_list_i).obs_ch);
-		existed_ind=find(strcmp([exam_obs.name num2str(exam_obs.ch)], existed_log.name),1);
-		if isempty(existed_ind)
-			exam_obs.rate=examine_one_obs_ch_case(base, exam_obs, pict, alg);
-		else
-			exam_obs.rate=existed_log.rate(existed_ind);
+	%% main features examination loop
+	if ~usepool
+		fh_log=fopen(log_file_name,'a');
+		for obs_name_ch_list_i=1:length(obs_name_ch_list)
+			exam_obs=struct('name',obs_name_ch_list(obs_name_ch_list_i).obs_name, 'ch',obs_name_ch_list(obs_name_ch_list_i).obs_ch);
+			existed_ind=find(strcmp([exam_obs.name num2str(exam_obs.ch)], existed_log.name),1);
+			if isempty(existed_ind)
+				exam_obs.rate=examine_one_obs_ch_case(base, exam_obs, pict, alg);
+			else
+				exam_obs.rate=existed_log.rate(existed_ind);
+			end
+			exam_log{obs_name_ch_list_i}=exam_obs;
+			fprintf(fh_log, '%0.5f %s%d\n', exam_obs.rate, exam_obs.name, exam_obs.ch);
 		end
-		exam_log{obs_name_ch_list_i}=exam_obs;
-		fprintf(fh_log, '%0.5f %s%d\n', exam_obs.rate, exam_obs.name, exam_obs.ch);
-	end
-	fclose(fh_log);
-
-	if usepool
+		fclose(fh_log);
+	else
+		parfor obs_name_ch_list_i=1:length(obs_name_ch_list)
+			exam_obs=struct('name',obs_name_ch_list(obs_name_ch_list_i).obs_name, 'ch',obs_name_ch_list(obs_name_ch_list_i).obs_ch);
+			existed_ind=find(strcmp([exam_obs.name num2str(exam_obs.ch)], existed_log.name),1);
+			if isempty(existed_ind)
+				exam_obs.rate=examine_one_obs_ch_case(base, exam_obs, pict, alg);
+			else
+				exam_obs.rate=existed_log.rate(existed_ind);
+			end
+			exam_log{obs_name_ch_list_i}=exam_obs;
+		end
 		matlabpool('close');
 	end
 
@@ -105,6 +123,10 @@ function classifier_rate=examine_one_obs_ch_case(base, exam_obs, pict, alg)
 	for cl_i=1:numel(base)
 		f_obs{cl_i}=cellfun(@(x) x.(exam_obs.name)(:,exam_obs.ch), base(cl_i).data, 'UniformOutput',false);
 	end
+
+	% Флаг того, что это скорее всего статистические данные -- для каждого
+	% файла есть только одно значение, а не вектор значений
+	is_oneobs = all(cellfun(@(x) size(x,1)==1, vertcat(f_obs{:})));
 
 	obs_pic_subdir='';
 	for pi=1:length(pict.obs_types)
@@ -130,15 +152,24 @@ function classifier_rate=examine_one_obs_ch_case(base, exam_obs, pict, alg)
 	if pict.make_pic_or_fig
 		fig=figure('Units','pixels', 'Position',pict.fig_sz, 'Visible','off', 'PaperPositionMode','auto');
 
-		fig_axes=zeros(2+numel(base),1);
-		sp_rows=ceil(numel(base)/2)+1;
-		for sp_i=0:1+numel(base)
-			fig_axes(sp_i+1)=axes('Parent',fig, 'Units','normalized', 'Position', ...
-				[0.05+rem(sp_i,2)*0.5, (sp_rows-1-fix(sp_i/2))/sp_rows+0.03, 0.44, 1/sp_rows-0.06]);
+		if is_oneobs
+			fig_axes=[	axes('Parent',fig, 'Units','normalized', 'Position', [0.05 0.53 0.94 0.44]);...
+						axes('Parent',fig, 'Units','normalized', 'Position', [0.05 0.03 0.94 0.44])];
+		else
+			fig_axes=zeros(2+numel(base),1);
+			sp_rows=ceil(numel(base)/2)+1;
+			for sp_i=0:1+numel(base)
+				fig_axes(sp_i+1)=axes('Parent',fig, 'Units','normalized', 'Position', ...
+					[0.05+rem(sp_i,2)*0.5, (sp_rows-1-fix(sp_i/2))/sp_rows+0.03, 0.44, 1/sp_rows-0.06]);
+			end
 		end
 	end
 
-	x.qnt=0.05;
+	if is_oneobs
+		x.qnt=0;
+	else
+		x.qnt=0.05;
+	end
 	x.rg=[inf -inf];
 
 	%% Plot PDF & CDF
@@ -150,46 +181,50 @@ function classifier_rate=examine_one_obs_ch_case(base, exam_obs, pict, alg)
 			x.rg(1)=min(x.rg(1),quantile(all_cur_cl_obs,x.qnt));
 			x.rg(2)=max(x.rg(2),quantile(all_cur_cl_obs,1-x.qnt));
 
-			[hy,hx]=hist(all_cur_cl_obs,100);
-			if ischar(base(cl_i).color)
-				plot(fig_axes(1), hx,hy/sum(hy), base(cl_i).color);
-			else
-				plot(fig_axes(1), hx,hy/sum(hy), 'Color',base(cl_i).color);
-			end
+			obs_edges = linspace( x.rg(1), x.rg(2), max(10,min(100,round(size(all_cur_cl_obs,1)/5))) );
+			hy=histc(all_cur_cl_obs, obs_edges);
+			hy(end)=0;
+			hy = hy / (diff(obs_edges)*hy(1:end-1));
+
+			stairs(fig_axes(1), obs_edges([1 1:end]), [0; hy], 'Color',base(cl_i).color,'LineWidth',1.5);
 			hold(fig_axes(1), 'on');
 		end
 
-		files_cdx=quantile(all_cur_cl_obs,linspace(x.qnt,1-x.qnt,300))';
-		files_cdf=zeros(1,numel(f_obs{cl_i}));
-
-		for file_i=1:numel(f_obs{cl_i})
-			file_obs=f_obs{cl_i}{file_i};
-
-			if not(isempty(file_obs))
-				cur_cdf=multi_cdf.fit(file_obs,{files_cdx});
-
-				if pict.make_pic_or_fig
-					cdf_x=(cur_cdf.cdfs.arg(1:end-1)+cur_cdf.cdfs.arg(2:end))/2;
-					if ischar(base(cl_i).color)
-						plot(fig_axes(2), cdf_x, cur_cdf.cdfs.cdf, base(cl_i).color);
-					else
-						plot(fig_axes(2), cdf_x, cur_cdf.cdfs.cdf, 'Color',base(cl_i).color);
-					end
-					hold(fig_axes(2), 'on');
-
-					plot(fig_axes(2+cl_i), cdf_x, cur_cdf.cdfs.cdf, 'Color',pict.palette(randi(size(pict.palette,1),1),:));
-					hold(fig_axes(2+cl_i), 'on');
-				end
-
-				files_cdf(1:size(cur_cdf.cdfs.cdf,1),file_i)=cur_cdf.cdfs.cdf;
-			else
-				fprintf('WARNING: no %s observations in file %s.\n',exam_obs.name,base(cl_i).data{file_i}.file_name);
+		if is_oneobs
+			if pict.make_pic_or_fig
+				[hy,hx]=ecdf(all_cur_cl_obs);
+				plot(fig_axes(2), hx, hy, 'Color',base(cl_i).color, 'LineWidth',1.5);
+				hold(fig_axes(2), 'on');
 			end
-		end
+		else
+			files_cdx=quantile(all_cur_cl_obs,linspace(x.qnt,1-x.qnt,250))';
+			files_cdf=zeros(1,numel(f_obs{cl_i}));
 
-		cl_cdf{cl_i}=multi_cdf();
-		cl_cdf{cl_i}.cdfs.arg=files_cdx;
-		cl_cdf{cl_i}.cdfs.cdf=median(files_cdf,2);
+			for file_i=1:numel(f_obs{cl_i})
+				file_obs=f_obs{cl_i}{file_i};
+
+				if not(isempty(file_obs))
+					cur_cdf=multi_cdf.fit(file_obs,{files_cdx});
+
+					if pict.make_pic_or_fig
+						cdf_x=(cur_cdf.cdfs.arg(1:end-1)+cur_cdf.cdfs.arg(2:end))/2;
+						plot(fig_axes(2), cdf_x, cur_cdf.cdfs.cdf, 'Color',base(cl_i).color);
+						hold(fig_axes(2), 'on');
+
+						plot(fig_axes(2+cl_i), cdf_x, cur_cdf.cdfs.cdf, 'Color',pict.palette(randi(size(pict.palette,1),1),:));
+						hold(fig_axes(2+cl_i), 'on');
+					end
+
+					files_cdf(1:size(cur_cdf.cdfs.cdf,1),file_i)=cur_cdf.cdfs.cdf;
+				else
+					fprintf('WARNING: no %s observations in file %s.\n',exam_obs.name,base(cl_i).data{file_i}.file_name);
+				end
+			end
+
+			cl_cdf{cl_i}=multi_cdf();
+			cl_cdf{cl_i}.cdfs.arg=files_cdx;
+			cl_cdf{cl_i}.cdfs.cdf=median(files_cdf,2);
+		end
 	end
 
 	if pict.make_pic_or_fig
@@ -207,57 +242,58 @@ function classifier_rate=examine_one_obs_ch_case(base, exam_obs, pict, alg)
 	end
 
 	%% Examine observations
-	svm_cl_name=cell(numel(f_obs),1);
-	svm_cl_dist=cell(numel(f_obs),1);
 
-	for cl_i=1:numel(f_obs)
-		if pict.make_pic_or_fig
-			cdf_x=(cl_cdf{cl_i}.cdfs.arg(1:end-1)+cl_cdf{cl_i}.cdfs.arg(2:end))/2;
-			if ischar(base(cl_i).color)
-				plot(fig_axes(2), cdf_x, cl_cdf{cl_i}.cdfs.cdf, base(cl_i).color, 'LineWidth',5);
-				plot(fig_axes(2), cdf_x, cl_cdf{cl_i}.cdfs.cdf, 'w', 'LineWidth',4);
-				plot(fig_axes(2), cdf_x, cl_cdf{cl_i}.cdfs.cdf, [base(cl_i).color '--'], 'LineWidth',3);
-			else
+	if is_oneobs
+		svm_cl_name = cellfun(@(x,y) zeros(numel(x),1)+y, f_obs(:), num2cell((1:numel(f_obs))'), 'UniformOutput',false);
+		svm_cl_dist = cellfun(@cell2mat, f_obs(:), 'UniformOutput',false);
+	else
+		svm_cl_name=cell(numel(f_obs),1);
+		svm_cl_dist=cell(numel(f_obs),1);
+
+		for cl_i=1:numel(f_obs)
+			if pict.make_pic_or_fig
+				cdf_x=(cl_cdf{cl_i}.cdfs.arg(1:end-1)+cl_cdf{cl_i}.cdfs.arg(2:end))/2;
 				plot(fig_axes(2), cdf_x, cl_cdf{cl_i}.cdfs.cdf, 'Color',base(cl_i).color, 'LineWidth',5);
 				plot(fig_axes(2), cdf_x, cl_cdf{cl_i}.cdfs.cdf, 'w', 'LineWidth',4);
 				plot(fig_axes(2), cdf_x, cl_cdf{cl_i}.cdfs.cdf, 'Color',base(cl_i).color, 'LineStyle','--', 'LineWidth',3);
+
+				if x.rg(1)<x.rg(2)
+					xlim(fig_axes(2+cl_i), x.rg);
+				end
+				hold(fig_axes(2+cl_i), 'off');
+				grid(fig_axes(2+cl_i), 'on');
+				ylabel(fig_axes(2+cl_i), 'CDF');
+				title(fig_axes(2+cl_i), base(cl_i).class, 'Interpreter','none');
 			end
 
-			if x.rg(1)<x.rg(2)
-				xlim(fig_axes(2+cl_i), x.rg);
-			end
-			hold(fig_axes(2+cl_i), 'off');
-			grid(fig_axes(2+cl_i), 'on');
-			ylabel(fig_axes(2+cl_i), 'CDF');
-			title(fig_axes(2+cl_i), base(cl_i).class, 'Interpreter','none');
-		end
+			svm_cl_name{cl_i}=zeros(numel(f_obs{cl_i}),1)+cl_i;
+			svm_cl_dist{cl_i}=zeros(numel(f_obs{cl_i}),numel(cl_cdf));
 
-		svm_cl_name{cl_i}=zeros(numel(f_obs{cl_i}),1)+cl_i;
-		svm_cl_dist{cl_i}=zeros(numel(f_obs{cl_i}),numel(cl_cdf));
-
-		for file_i=1:numel(f_obs{cl_i})
-			file_obs=f_obs{cl_i}{file_i};
-			file_obs(isnan(file_obs))=[];
-			if not(isempty(file_obs))
-				for cl_cdf_i=1:numel(cl_cdf)
-					svm_cl_dist{cl_i}(file_i,cl_cdf_i)=cl_cdf{cl_cdf_i}.distance(file_obs);
+			for file_i=1:numel(f_obs{cl_i})
+				file_obs=f_obs{cl_i}{file_i};
+				file_obs(isnan(file_obs))=[];
+				if not(isempty(file_obs))
+					for cl_cdf_i=1:numel(cl_cdf)
+						svm_cl_dist{cl_i}(file_i,cl_cdf_i)=cl_cdf{cl_cdf_i}.distance(file_obs);
+					end
 				end
 			end
 		end
 	end
 
 	svm_cl_name=cell2mat(svm_cl_name);
-	svm_cl_dist=cell2mat(svm_cl_dist);
+	svm_cl_dist=double(cell2mat(svm_cl_dist));
 
-	svm_opt_arg = ' -h 0 -q';
+	svm_opt_arg = ' -h 0 -q -v 10';
+	svm_weigth_arg = '';
 	base_sz=arrayfun(@(x) numel(x.data), base);
-	if alg.examine_obs.base_auto_balance
-		svm_opt_arg = [svm_opt_arg cell2mat(arrayfun(@(x,y) sprintf(' -w%d %e',x,y), 1:numel(base_sz), min(base_sz)./base_sz', 'UniformOutput',false))];
-	end
+	svm_weigth_arg = [svm_weigth_arg cell2mat(arrayfun(@(x,y) sprintf(' -w%d %e',x,y), 1:numel(base_sz), min(base_sz)./base_sz', 'UniformOutput',false))];
 
-	[~,~,svm_cl_res]=lib_svm.find_cost_gamma(svm_cl_dist, svm_cl_name, svm_opt_arg);
+	svm_cl_res = libsvmtrain(svm_cl_name, svm_cl_dist, svm_opt_arg);
+	rate_accuracy = lib_svm.rate_prediction(svm_cl_name, svm_cl_res);
 
-	[rate_accuracy, rate_avr_recall] = lib_svm.rate_prediction(svm_cl_name, svm_cl_res);
+	svm_cl_res = libsvmtrain(svm_cl_name, svm_cl_dist, [svm_opt_arg svm_weigth_arg]);
+	[~, rate_avr_recall] = lib_svm.rate_prediction(svm_cl_name, svm_cl_res);
 
 	switch(alg.examine_obs.objective_func)
 		case 'accuracy'
